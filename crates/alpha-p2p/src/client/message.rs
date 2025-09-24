23396f52872490172dbe58b1d0238c3c0a854e04
Joshua J. Bouw <jjb@unicity-labs.com>
2025-09-25 00:03:23 +0400

    feat: sketch out required messages

--- crates/alpha-p2p/src/client/message.rs
+++ crates/alpha-p2p/src/client/message.rs
@@ -1,10 +1,33 @@
 // See: https://developer.bitcoin.org/reference/p2p_networking.html#data-messages
 // Only requests are implement as this client is not meant to relay.
 
-mod get_blocks;
 mod get_data;
+mod get_headers;
 mod inventory;
 
+use get_headers::GetHeaders;
+
 pub enum Message {
-    GetBlocks(get_blocks::GetBlocks),
+    Connection(Connection),
+    Request(Request),
+    Response(Response),
+}
+
+pub enum Connection {
+    Version,
+    VerAck,
+    Ping,
+    Pong,
+    SendHeaders,
+}
+
+pub enum Request {
+    GetHeaders(GetHeaders),
+}
+
+pub enum Response {
+    Headers,
+    Block,
+    Tx,
+    NotFound,
 }
