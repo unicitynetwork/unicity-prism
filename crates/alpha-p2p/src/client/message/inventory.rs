7eda1782405862411196ed4d1583c79d4e20dd81
Joshua J. Bouw <jjb@unicity-labs.com>
2025-09-24 20:44:28 +0400

    feat: add consensus encoding for a vector of inventories

--- crates/alpha-p2p/src/client/message/inventory.rs
+++ crates/alpha-p2p/src/client/message/inventory.rs
@@ -1,9 +1,82 @@
 use crate::blockdata::block::BlockHash;
 use crate::blockdata::transaction::Txid;
+use crate::consensus::encode::VarInt;
 use crate::consensus::{Decodable, Encodable};
 use crate::hashes::Hash;
 
+/// A list of inventory items for consensus encoding
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub(crate) struct InventoryList(pub Vec<Inventory>);
+
+impl InventoryList {
+    /// Creates a new `InventoryList` from a vector of `Inventory` items.
+    pub fn new(inventories: Vec<Inventory>) -> Self {
+        Self(inventories)
+    }
+
+    /// Consumes the `InventoryList` and returns the underlying vector of `Inventory` items.
+    pub fn into_vec(self) -> Vec<Inventory> {
+        self.0
+    }
+
+    /// Returns a slice of the underlying vector of `Inventory` items.
+    pub fn as_slice(&self) -> &[Inventory] {
+        &self.0
+    }
+}
+
+impl From<Vec<Inventory>> for InventoryList {
+    fn from(vec: Vec<Inventory>) -> Self {
+        Self(vec)
+    }
+}
+
+impl From<InventoryList> for Vec<Inventory> {
+    fn from(list: InventoryList) -> Self {
+        list.0
+    }
+}
+
+#[allow(
+    clippy::arithmetic_side_effects,
+    reason = "Length calculations can't overflow in practice"
+)]
+impl Encodable for InventoryList {
+    fn consensus_encode<W: crate::io::Write + ?Sized>(
+        &self,
+        w: &mut W,
+    ) -> Result<usize, crate::io::Error> {
+        let mut len = 0;
+        len += VarInt::from(self.0.len()).consensus_encode(w)?;
+        for item in &self.0 {
+            len += item.consensus_encode(w)?;
+        }
+        Ok(len)
+    }
+}
+
+impl Decodable for InventoryList {
+    fn consensus_decode<R: crate::io::Read + ?Sized>(
+        r: &mut R,
+    ) -> Result<Self, crate::consensus::EncodeDecodeError> {
+        let len = VarInt::consensus_decode(r)?.0;
+
+        if len > crate::consensus::MAX_VEC_SIZE as u64 {
+            return Err(crate::consensus::EncodeDecodeError::ParseFailed(
+                "Vector too large, exceeds MAX_VEC_SIZE",
+            ));
+        }
+
+        #[allow(clippy::cast_possible_truncation, reason = "Already pre-checked")]
+        let mut ret = Vec::with_capacity(len as usize);
+        for _ in 0..len {
+            ret.push(Inventory::consensus_decode(r)?);
+        }
+        Ok(InventoryList(ret))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Inventory {
     /// The hash is a `Txid`.
     Transaction(Txid),
@@ -147,6 +220,195 @@ mod tests {
             hex_to_hash("de55ffd709ac1f5dc509a0925d0b1fc442ca034f224732e429081da1b621f55a")?;
 
         assert_eq!(expected_inv, Inventory::Transaction(expected_txid));
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_inventory_list_encode_single() -> Result<(), Box<dyn std::error::Error>> {
+        let txid: Txid =
+            hex_to_hash("de55ffd709ac1f5dc509a0925d0b1fc442ca034f224732e429081da1b621f55a")?;
+
+        let inventory_list = InventoryList::new(vec![Inventory::Transaction(txid)]);
+
+        let mut encoded = Vec::new();
+        let _bytes_written = inventory_list.consensus_encode(&mut encoded)?;
+
+        // Expected: VarInt(1) + single inventory item
+        let expected = hex::decode(
+            "01\
+            01000000\
+            de55ffd709ac1f5dc509a0925d0b1fc4\
+            42ca034f224732e429081da1b621f55a",
+        )?;
+
+        assert_eq!(expected, encoded);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_inventory_list_decode_single() -> Result<(), Box<dyn std::error::Error>> {
+        let expected_data = hex::decode(
+            "01\
+            01000000\
+            de55ffd709ac1f5dc509a0925d0b1fc4\
+            42ca034f224732e429081da1b621f55a",
+        )?;
+
+        let mut cursor = std::io::Cursor::new(&expected_data);
+        let decoded_list = InventoryList::consensus_decode(&mut cursor)?;
+
+        let expected_txid: Txid =
+            hex_to_hash("de55ffd709ac1f5dc509a0925d0b1fc442ca034f224732e429081da1b621f55a")?;
+
+        assert_eq!(decoded_list.0.len(), 1);
+        assert_eq!(
+            *decoded_list.0.first().ok_or("First doesn't exist")?,
+            Inventory::Transaction(expected_txid)
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_inventory_list_encode_multiple() -> Result<(), Box<dyn std::error::Error>> {
+        let txid: Txid =
+            hex_to_hash("de55ffd709ac1f5dc509a0925d0b1fc442ca034f224732e429081da1b621f55a")?;
+        let block_hash: BlockHash =
+            hex_to_hash("00000000000000000007878ec04bb2543ee2c5f9ce6d8b9c8d2d41a5b17db6a6")?;
+
+        let inventory_list = InventoryList::new(vec![
+            Inventory::Transaction(txid),
+            Inventory::Block(block_hash),
+        ]);
+
+        let mut encoded = Vec::new();
+        let _bytes_written = inventory_list.consensus_encode(&mut encoded)?;
+
+        // Expected: VarInt(2) + two inventory items
+        let expected = hex::decode(
+            "02\
+            01000000\
+            de55ffd709ac1f5dc509a0925d0b1fc4\
+            42ca034f224732e429081da1b621f55a\
+            02000000\
+            00000000000000000007878ec04bb254\
+            3ee2c5f9ce6d8b9c8d2d41a5b17db6a6",
+        )?;
+
+        assert_eq!(expected, encoded);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_inventory_list_decode_multiple() -> Result<(), Box<dyn std::error::Error>> {
+        let expected_data = hex::decode(
+            "02\
+            01000000\
+            de55ffd709ac1f5dc509a0925d0b1fc4\
+            42ca034f224732e429081da1b621f55a\
+            02000000\
+            00000000000000000007878ec04bb254\
+            3ee2c5f9ce6d8b9c8d2d41a5b17db6a6",
+        )?;
+
+        let mut cursor = std::io::Cursor::new(&expected_data);
+        let decoded_list = InventoryList::consensus_decode(&mut cursor)?;
+
+        let expected_txid: Txid =
+            hex_to_hash("de55ffd709ac1f5dc509a0925d0b1fc442ca034f224732e429081da1b621f55a")?;
+        let expected_block: BlockHash =
+            hex_to_hash("00000000000000000007878ec04bb2543ee2c5f9ce6d8b9c8d2d41a5b17db6a6")?;
+
+        assert_eq!(decoded_list.0.len(), 2);
+        assert_eq!(
+            *decoded_list.0.first().ok_or("First doesn't exist")?,
+            Inventory::Transaction(expected_txid)
+        );
+        assert_eq!(
+            *decoded_list.0.get(1).ok_or("Index 1 doesn't exist")?,
+            Inventory::Block(expected_block)
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_inventory_list_encode_empty() -> Result<(), Box<dyn std::error::Error>> {
+        let inventory_list = InventoryList::new(vec![]);
+
+        let mut encoded = Vec::new();
+        let _bytes_written = inventory_list.consensus_encode(&mut encoded)?;
+
+        // Expected: Just VarInt(0)
+        let expected = vec![0x00];
+
+        assert_eq!(expected, encoded);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_inventory_list_decode_empty() -> Result<(), Box<dyn std::error::Error>> {
+        let expected_data = vec![0x00];
+
+        let mut cursor = std::io::Cursor::new(&expected_data);
+        let decoded_list = InventoryList::consensus_decode(&mut cursor)?;
+
+        assert_eq!(decoded_list.0.len(), 0);
+        assert!(decoded_list.0.is_empty());
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_inventory_list_roundtrip() -> Result<(), Box<dyn std::error::Error>> {
+        let txid: Txid =
+            hex_to_hash("de55ffd709ac1f5dc509a0925d0b1fc442ca034f224732e429081da1b621f55a")?;
+        let block_hash: BlockHash =
+            hex_to_hash("00000000000000000007878ec04bb2543ee2c5f9ce6d8b9c8d2d41a5b17db6a6")?;
+
+        let original_list = InventoryList::new(vec![
+            Inventory::Transaction(txid),
+            Inventory::Block(block_hash),
+            Inventory::WitnessTransaction(txid),
+        ]);
+
+        // Encode
+        let mut encoded = Vec::new();
+        let _bytes_written = original_list.consensus_encode(&mut encoded)?;
+
+        // Decode
+        let mut cursor = std::io::Cursor::new(&encoded);
+        let decoded_list = InventoryList::consensus_decode(&mut cursor)?;
+
+        // Verify roundtrip
+        assert_eq!(original_list, decoded_list);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_inventory_list_conversions() -> Result<(), Box<dyn std::error::Error>> {
+        let txid: Txid =
+            hex_to_hash("de55ffd709ac1f5dc509a0925d0b1fc442ca034f224732e429081da1b621f55a")?;
+
+        let original_vec = vec![Inventory::Transaction(txid)];
+
+        // Test From<Vec<Inventory>>
+        let inventory_list: InventoryList = original_vec.clone().into();
+        assert_eq!(inventory_list.0, original_vec);
+
+        // Test into_vec()
+        let converted_vec = inventory_list.into_vec();
+        assert_eq!(converted_vec, original_vec);
+
+        // Test as_slice()
+        let new_list = InventoryList::new(original_vec.clone());
+        let slice = new_list.as_slice();
+        assert_eq!(slice, &original_vec[..]);
 
         Ok(())
     }
