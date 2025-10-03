32355ed331a835cf3a3d8406973549cc1589ae76
Joshua J. Bouw <jjb@unicity-labs.com>
2025-10-03 20:57:05 +0400

    doc: add to blockdata

--- crates/alpha-p2p/src/blockdata.rs
+++ crates/alpha-p2p/src/blockdata.rs
@@ -1,2 +1,8 @@
+/// Block-related data structures and functionality.
 pub mod block;
+
+/// Genesis block information for Unicity networks.
+pub mod genesis;
+
+/// Transaction-related data structures and functionality.
 pub mod transaction;
